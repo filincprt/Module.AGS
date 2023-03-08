b04beb388205af93039b5d710814d1db460c6284
Филин Виталий <vitalyfilin19@gmail.com>
2023-03-08 20:57:00 +0500

    No work - сохр ради кода

--- Module.AGS/GasStationJSON.cs
+++ Module.AGS/GasStationJSON.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,36 @@ namespace Module.AGS
 {
     public class StationInfo
     {
+        [JsonProperty("station_id")]
         public int StationId { get; set; }
+
+        [JsonProperty("address")]
         public string Address { get; set; }
+
+        [JsonProperty("price_AI92")]
         public decimal PriceAI92 { get; set; }
+
+        [JsonProperty("price_AI95")]
         public decimal PriceAI95 { get; set; }
+
+        [JsonProperty("price_AI98")]
         public decimal PriceAI98 { get; set; }
+
+        [JsonProperty("price_diesel")]
         public decimal PriceDiesel { get; set; }
+
+        [JsonProperty("remainder_AI92")]
         public int RemainderAI92 { get; set; }
+
+        [JsonProperty("remainder_AI95")]
         public int RemainderAI95 { get; set; }
+
+        [JsonProperty("remainder_AI98")]
         public int RemainderAI98 { get; set; }
+
+        [JsonProperty("remainder_diesel")]
         public int RemainderDiesel { get; set; }
     }
+
 
 }
--- Module.AGS/MainWindow.xaml.cs
+++ Module.AGS/MainWindow.xaml.cs
@@ -3,65 +3,35 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Module.AGS
 {
     public partial class MainWindow : Window
     {
-        private string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
-        private int currentStationID = 0;
-
         public MainWindow()
         {
             InitializeComponent();
-          
         }
-
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
-            int id = Convert.ToInt32(txtIdStation.Text);
-            GetAndDisplayStationInfo(id);
-
-
-            /*
-            int stationID = int.Parse(txtIdStation.Text);
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            int id = Convert.ToInt32(txtIdStation.ToString());
+            if (id == 0)
             {
-                connection.Open();
-                string queryString = "SELECT * FROM gas_stations WHERE station_id = @stationID";
-                using (SqlCommand command = new SqlCommand(queryString, connection))
-                {
-                    command.Parameters.AddWithValue("@stationID", stationID);
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            txtAddress.Text = reader["address"].ToString();
-                            txtPriceAI92.Text = reader["price_AI92"].ToString();
-                            txtPriceAI95.Text = reader["price_AI95"].ToString();
-                            txtPriceAI98.Text = reader["price_AI98"].ToString();
-                            txtPriceDiesel.Text = reader["price_diesel"].ToString();
-                            txtRemainderAI92.Text = reader["remainder_AI92"].ToString();
-                            txtRemainderAI95.Text = reader["remainder_AI95"].ToString();
-                            txtRemainderAI98.Text = reader["remainder_AI98"].ToString();
-                            txtRemainderDiesel.Text = reader["remainder_diesel"].ToString();
-                            currentStationID = stationID;
-                        }
-                        else
-                        {
-                            MessageBox.Show("Station not found");
-                        }
-                    }
-                }
+                MessageBox.Show("Заполните поле Station Id!");
             }
-            */
-
+            else
+                _ = GetAndDisplayStationInfo(id);
+            
         }
 
         private async Task GetAndDisplayStationInfo(int id)
@@ -83,36 +53,65 @@ namespace Module.AGS
             }
         }
 
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            int id = Convert.ToInt32(txtIdStation.Text);
+
+            StationInfo newStationInfo = new StationInfo
             {
-                connection.Open();
-                string queryString = "UPDATE gas_stations SET address=@address, price_AI92=@price_AI92, price_AI95=@price_AI95, price_AI98=@price_AI98, price_diesel=@price_diesel, remainder_AI92=@remainder_AI92, remainder_AI95=@remainder_AI95, remainder_AI98=@remainder_AI98, remainder_diesel=@remainder_diesel WHERE station_id=@stationID";
-                using (SqlCommand command = new SqlCommand(queryString, connection))
+                StationId = id,
+                Address = txtAddress.Text,
+                PriceAI92 = decimal.Parse(txtPriceAI92.Text),
+                PriceAI95 = decimal.Parse(txtPriceAI95.Text),
+                PriceAI98 = decimal.Parse(txtPriceAI98.Text),
+                PriceDiesel = decimal.Parse(txtPriceDiesel.Text),
+                RemainderAI92 = int.Parse(txtRemainderAI92.Text),
+                RemainderAI95 = int.Parse(txtRemainderAI95.Text),
+                RemainderAI98 = int.Parse(txtRemainderAI98.Text),
+                RemainderDiesel = int.Parse(txtRemainderDiesel.Text)
+            };
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:50682/getStationinfo/{id}");
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var originalStationInfo = JsonConvert.DeserializeObject<StationInfo>(jsonString);
+
+                if (newStationInfo.Address != originalStationInfo.Address
+                    || newStationInfo.PriceAI92 != originalStationInfo.PriceAI92
+                    || newStationInfo.PriceAI95 != originalStationInfo.PriceAI95
+                    || newStationInfo.PriceAI98 != originalStationInfo.PriceAI98
+                    || newStationInfo.PriceDiesel != originalStationInfo.PriceDiesel
+                    || newStationInfo.RemainderAI92 != originalStationInfo.RemainderAI92
+                    || newStationInfo.RemainderAI95 != originalStationInfo.RemainderAI95
+                    || newStationInfo.RemainderAI98 != originalStationInfo.RemainderAI98
+                    || newStationInfo.RemainderDiesel != originalStationInfo.RemainderDiesel)
                 {
-                    command.Parameters.AddWithValue("@address", txtAddress.Text);
-                    command.Parameters.AddWithValue("@price_AI92", float.Parse(txtPriceAI92.Text));
-                    command.Parameters.AddWithValue("@price_AI95", float.Parse(txtPriceAI95.Text));
-                    command.Parameters.AddWithValue("@price_AI98", float.Parse(txtPriceAI98.Text));
-                    command.Parameters.AddWithValue("@price_diesel", float.Parse(txtPriceDiesel.Text));
-                    command.Parameters.AddWithValue("@remainder_AI92", int.Parse(txtRemainderAI92.Text));
-                    command.Parameters.AddWithValue("@remainder_AI95", int.Parse(txtRemainderAI95.Text));
-                    command.Parameters.AddWithValue("@remainder_AI98", int.Parse(txtRemainderAI98.Text));
-                    command.Parameters.AddWithValue("@remainder_diesel", int.Parse(txtRemainderDiesel.Text));
-                    command.Parameters.AddWithValue("@stationID", currentStationID);
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        MessageBox.Show("Changes saved");
-                    }
-                    else
-                    {
-                        MessageBox.Show("No changes were made");
-                    }
+                    var content = new StringContent(JsonConvert.SerializeObject(newStationInfo), Encoding.UTF8, "application/json");
+                    var result = await client.PostAsync($"http://localhost:50682/SetStation/{id}", content);
+                    await GetAndDisplayStationInfo(id);
                 }
             }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var addStation = new StationInfo()
+            {
+                Address = txtAddress.Text,
+                PriceAI92 = decimal.Parse(txtPriceAI92.Text),
+                PriceAI95 = decimal.Parse(txtPriceAI95.Text),
+                PriceAI98 = decimal.Parse(txtPriceAI98.Text),
+                PriceDiesel = decimal.Parse(txtPriceDiesel.Text),
+                RemainderAI92 = int.Parse(txtRemainderAI92.Text),
+                RemainderAI95 = int.Parse(txtRemainderAI95.Text),
+                RemainderAI98 = int.Parse(txtRemainderAI98.Text),
+                RemainderDiesel = int.Parse(txtRemainderDiesel.Text)
+            };
+
+            string url = "http://localhost:50682/AddStation";
+           
+            
         }
     }
 }
